1894b72ad5cf926559a290b69ba9ea168a732c88
Sayan J Chatterjee <sayanc797@IN.nam.ad.pwcinternal.com>
2018-03-14 19:39:52 +0530

    This is the initial commit for the devops demo

---
+++ src/main/java/com/demo/CustomConfiguration.java
@@ -0,0 +1,16 @@
+package com.demo;
+//package com.demo.pckg;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.demo.repository.PersonRepository;
+//
+//@Configuration
+//public class CustomConfiguration {
+//	
+//	@Bean
+//	public PersonRepository getPersonRepository(){
+//		
+//	}
+//}
